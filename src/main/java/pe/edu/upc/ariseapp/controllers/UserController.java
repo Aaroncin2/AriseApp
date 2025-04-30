8e3680afa2e111e7d242149a742f8f794308ca2d
Pierogabulle <u20231E145@upc.edu.pe>
2025-04-29 20:44:10 -0500

    Services de Recompensas, Rol, TypeDonation, Volunteering, creados.

--- src/main/java/pe/edu/upc/ariseapp/controllers/UserController.java
+++ src/main/java/pe/edu/upc/ariseapp/controllers/UserController.java
@@ -13,5 +13,4 @@ import java.util.stream.Collectors;
 @RequestMapping("/users")
 public class UserController {
 
-
 }
--- src/main/java/pe/edu/upc/ariseapp/servicesinterfaces/IRolService.java
+++ src/main/java/pe/edu/upc/ariseapp/servicesinterfaces/IRolService.java
@@ -1,5 +1,14 @@
 package pe.edu.upc.ariseapp.servicesinterfaces;
 
+import pe.edu.upc.ariseapp.entities.Rol;
+
+import java.util.List;
+
 public interface IRolService {
+    public List<Rol> list();
+    public void insert(Rol r);
+    public Rol listId(int idRol);
+    public void update(Rol r);
+    public void delete(int idRol);
 
 }
